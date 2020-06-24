53cc6bfc6d52c6a9482921e2c3870d5fd47b7299
Apoorv Garg <u.garg.10@gmail.com>
2020-06-24 22:48:24 +0530

    First Commit

---
+++ counter/app.js
@@ -0,0 +1,30 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+
+    btn.addEventListener("click", function(e) {
+        console.log(e.currentTarget.classList);
+
+        const styles = e.currentTarget.classList;
+
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green"
+        } else if (count < 0) {
+            value.style.color = "red"
+        } else {
+            value.style.color = "black"
+        }
+        value.textContent = count;
+    })
+})
